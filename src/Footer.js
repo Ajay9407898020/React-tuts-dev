3efca78e4f0eefd3618a9ea193c4c059dfdf2ef2
Ajay_Wanjari <66022546+Ajay9407898020@users.noreply.github.com>
2023-01-07 13:07:35 +0530

    create functional component like header content footer

---
+++ src/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Footer() {
+    const today = new Date();
+    return <footer>
+        <p>Copyright &copy; { today.getFullYear()}</p>
+    </footer>;
+}
+
+export default Footer;
